8c8fcbeb17ca2d33d0386a74db5517a7ce0fa91f
ColeShirakata <coleshirakata@gmail.com>
2022-11-05 15:57:50 -0700

    Implemented basic functionality for bot

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import os
+import random
 import discord
-
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('TOKEN')
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
 
-client = discord.Client()
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+  print(f'{client.user} has connected to Discord!')
+
+TIPS = ["Drink more water!", "Eat more protein!", "Remember to use proper form!"]
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content == 'tip':
+    response = random.choice(TIPS)
+    await message.channel.send(response)
+
+client.run(TOKEN)

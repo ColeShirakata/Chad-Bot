81f055002e90e1fcf423de2a211b273ac5af9b1e
taxjames123@gmail.com <taxjames123@gmail.com>
2022-11-05 17:25:09 -0700

    Made changes to gym features

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ async def on_ready():
   print(f'{client.user} has connected to Discord!')
 
 TIPS = ["Drink more water!", "Eat more protein!", "Remember to use proper form!"]
+SPLITS = ["Push", "Pull", "Legs"]
 
 @client.event
 async def on_message(message):
@@ -26,5 +27,11 @@ async def on_message(message):
   if message.content == 'tip':
     response = random.choice(TIPS)
     await message.channel.send(response)
+
+  if message.content == 'gym':
+    response = random.choice(SPLITS)
+    await message.channel.send(response)
+    if response == "Push":
+      await message.channel.send("1. Bench Press\nIncline Dumbbells\n")
 
 client.run(TOKEN)
